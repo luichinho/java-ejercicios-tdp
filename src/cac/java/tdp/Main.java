8b51c8d05b341781e9798ceced3090d19d7b8680
luichinho <luis.08.05@gmail.com>
2017-08-10 20:12:59 -0300

    verificar si el numero es par

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -5,6 +5,7 @@
  * Utiliza el operador condicional ( ? : ) dentro del println para resolverlo. 
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -16,6 +17,19 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        int A;
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("ingrese un numero entero");
+        A = teclado.nextInt();
+        
+       /* if((A%2) == 0)
+            System.out.println("el numero es par");
+        else
+            System.out.println("el numero es impar");     
+        */
+        System.out.println(A%2 == 0 ? "el numero es par":"el es impar");
         
     }
     
