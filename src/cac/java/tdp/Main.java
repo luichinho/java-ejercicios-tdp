2584963b719a7f2c6ac5054e343443427945f663
luichinho <luis.08.05@gmail.com>
2017-08-10 20:48:41 -0300

    dias de la semana

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -16,20 +16,43 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static void dia(int dia_de_semana){
+        
+        switch(dia_de_semana){
+            case 1:
+                    System.out.println("lunes");
+                    break;
+            case 2:
+                    System.out.println("martes");
+                    break;
+            case 3:
+                    System.out.println("Miercoles");
+                    break;
+            case 4:
+                    System.out.println("jueves");
+                    break;
+            case 5:
+                    System.out.println("viernes");
+                    break;
+            case 6:
+                    System.out.println("sabado");
+                    break;
+            case 7:
+                   System.out.println("domingo");
+                    break;
+        } 
+    }
     public static void main(String[] args) {
         
         int A;
         Scanner teclado = new Scanner(System.in);
         
-        System.out.println("ingrese un numero entero");
+        System.out.println("ingrese un numero entero entre 1 a 7");
         A = teclado.nextInt();
         
-       /* if((A%2) == 0)
-            System.out.println("el numero es par");
-        else
-            System.out.println("el numero es impar");     
-        */
-        System.out.println(A%2 == 0 ? "el numero es par":"el es impar");
+        if(A < 0 || A > 7)
+            System.err.println("el numero ingresado es erroneo");
+        dia(A);
         
     }
     
