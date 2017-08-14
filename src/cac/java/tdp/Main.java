99632193125e559feb86b70a3a3785d4032c45be
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 21:49:27 -0300

    ejercicio 30

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 30: Programa que lea una variable entera mes y compruebe si el
+ * valor corresponde a un mes de 30, 31 o 28 días. Se mostrará además el nombre
+ * del mes. Se debe comprobar que el valor introducido esté comprendido entre 1 y 12.
  */
 package cac.java.tdp;
 
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
