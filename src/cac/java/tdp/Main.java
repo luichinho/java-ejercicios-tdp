22a6b55b81fad0dcd61b512f9d33ea5cbf5450b8
luichinho <luis.08.05@gmail.com>
2017-08-15 20:41:21 -0300

    ver dias deĺ mes

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -4,6 +4,7 @@
  * del mes. Se debe comprobar que el valor introducido esté comprendido entre 1 y 12.
  */
 package cac.java.tdp;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +15,70 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static void verCantidadDeDias(int mes){
+        
+        if(mes == 2 )
+            System.out.println("el mes tiene 28 dias");
+        else if(mes==1 || mes==3 || mes==7 || mes==8 ||mes==10|| mes==12)
+            System.out.println("el mes tiene 31 dias");
+        else 
+            System.out.println("el mes tiene 30 dias");
+    } 
+    
+    public static void mostrarMes(int mes){
+         switch(mes){
+            case 1:
+                    System.out.println("ENERO");
+                    break;
+            case 2:
+                    System.out.println("Febrero");
+                    break;
+            case 3:
+                    System.out.println("Marzo");
+                    break;
+            case 4:
+                    System.out.println("Abril");
+                    break;
+            case 5:
+                    System.out.println("MAYO");
+                    break;
+            case 6:
+                    System.out.println("Junio");
+                    break;
+            case 7:
+                    System.out.println("JULIO");
+                    break;
+            case 8:
+                    System.out.println("Agosto");
+                    break;
+            case 9:
+                    System.out.println("SEPTIEMBRE");
+                    break;
+            case 10:
+                    System.out.println("OCTUBRE");
+                    break;
+            case 11:
+                    System.out.println("Nobiembre");
+                    break;
+            case 12:
+                    System.out.println("DICIEMBRE");
+                    break;
+         }
+        
+    }
     public static void main(String[] args) {
         
+        int mes;        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("ingrese el numero de un mes valido(1 a 12)");
+        mes = teclado.nextInt();
+        if(mes < 1 || mes > 12)
+            System.err.println("el numero del mes es incorrecto");
+        else{
+            verCantidadDeDias(mes);
+            mostrarMes(mes);            
+        }
     }
     
 }
